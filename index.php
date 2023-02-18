ffa02fb540c25467a40a9d682134e64ad89914e6
Nick <pawsey18@gmail.com>
2023-02-17 23:09:17 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav>
+
+        <label class="logo">W0School</label>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Admission</a></li>
+            <li><a href="#" class="btn btn-success">Login</a></li>
+        </ul>
+    </nav>
+
+    <div class="section1">
+        <label class="img-text">Teaching The People Since 2007</label>
+        <img class="main-img" src="images/graphic.jpg" alt="">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="welcome" src="images/school2.jpg" alt="">
+
+            </div>
+            <div class="col-md-8">
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
